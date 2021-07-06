f6b473bcce9f1aa090e3f05d505418d6144823c2
Alex Batts <alex.batts05@gmail.com>
2021-07-06 17:50:37 -0400

    new warn message

--- src/main/java/admin/Warn.java
+++ src/main/java/admin/Warn.java
@@ -16,14 +16,24 @@ public class Warn extends ListenerAdapter {
         if (event.getMessage().getContentRaw().startsWith(Main.COMMAND_PREFIX + "warn")){
             if (event.getMessage().getMember().hasPermission(Permission.MANAGE_SERVER)){
 
+                List<Member> members = event.getMessage().getMentionedMembers();
+
+                String[] split = event.getMessage().getContentRaw().split("\\s+");
+                String message = "";
+                for (int i = members.size(); i < split.length; i ++ )
+                    message += split[i] + "";
+
                 EmbedBuilder warning = new EmbedBuilder();
-                warning.setAuthor("you have received a warning from " + event.getMessage().getGuild().getName());
+                warning.setAuthor("Warning");
+                warning.setTitle("You have received a warning from " + event.getMessage().getGuild().getName());
+                warning.setDescription("Message: " + message);
                 warning.setColor(Color.red);
 
-                List<Member> members = event.getMessage().getMentionedMembers();
                 for (Member member : members){
                     member.getUser().openPrivateChannel().flatMap(channel -> channel.sendMessage(warning.build())).queue();
                 }
+
+
 
                 if (members.size() > 0){
                     EmbedBuilder embed = new EmbedBuilder();
