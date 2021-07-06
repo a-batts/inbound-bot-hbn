10914db8ed0d70b0cc5201b36175fa13bcbd5cf3
Alex Batts <alex.batts05@gmail.com>
2021-07-06 18:33:09 -0400

    hotdonald

--- src/main/java/main/HBNCommands.java
+++ src/main/java/main/HBNCommands.java
@@ -1,5 +1,6 @@
 package main;
 
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -14,6 +15,12 @@ public class HBNCommands extends ListenerAdapter {
 
         if (message.getContentRaw().startsWith(Main.COMMAND_PREFIX + "lick"))
             message.reply("\uD83D\uDC45" + message.getMember().getEffectiveName() + " licked " + message.getContentRaw().substring(Main.COMMAND_PREFIX.length() + 5) + "\uD83D\uDC45").queue();
+
+        if (message.getContentRaw().equals(Main.COMMAND_PREFIX + "hotdonald")){
+            EmbedBuilder image = new EmbedBuilder();
+            image.setImage("https://i.kym-cdn.com/entries/icons/original/000/023/327/trumptennisss.jpg");
+            message.reply(image.build()).queue();
+        }
 
         if (message.getContentRaw().startsWith(Main.COMMAND_PREFIX + "stock")){
 
