65113606a8a06b3686128be0c7dc2141af04b76c
Alex Batts <alex.batts05@gmail.com>
2021-07-06 19:33:22 -0400

    unmute user

--- src/main/java/admin/Mute.java
+++ src/main/java/admin/Mute.java
@@ -16,7 +16,7 @@ public class Mute extends ListenerAdapter {
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
         if (event.getMessage().getContentRaw().startsWith(Main.COMMAND_PREFIX + "muteuser")){
-            if (event.getMessage().getMember().hasPermission(Permission.MANAGE_ROLES)|| event.getMessage().getMember().hasPermission(Permission.MANAGE_SERVER)){
+            if (event.getMessage().getMember().hasPermission(Permission.MANAGE_ROLES)){
                 List<Member> members = event.getMessage().getMentionedMembers();
 
                 try{
@@ -51,6 +51,39 @@ public class Mute extends ListenerAdapter {
             }
         }
         if (event.getMessage().getContentRaw().startsWith(Main.COMMAND_PREFIX + "unmuteuser")){
+            if (event.getMessage().getMember().hasPermission(Permission.MANAGE_ROLES)){
+                List<Member> members = event.getMessage().getMentionedMembers();
+
+                try{
+                    Role muteRole = event.getGuild().getRolesByName(Main.MUTE_CHANNEL, false).get(0);
+                    for(Member member: members){
+                        event.getGuild().removeRoleFromMember(member, muteRole).queue();
+                    }
+                    EmbedBuilder embed = new EmbedBuilder();
+                    embed.setAuthor("unmuted the mentioned users <3", null, event.getJDA().getSelfUser().getAvatarUrl());
+                    embed.setColor(Color.green);
+                    event.getMessage().getChannel().sendMessage(embed.build()).queue();
+                }
+                catch (IndexOutOfBoundsException e) {
+                    EmbedBuilder embed = new EmbedBuilder();
+                    embed.setAuthor("you don't have a role named \"muted\" on this server", null, event.getJDA().getSelfUser().getAvatarUrl());
+                    embed.setColor(Color.red);
+                    event.getMessage().getChannel().sendMessage(embed.build()).queue();
+                }
+                catch (InsufficientPermissionException e){
+                    EmbedBuilder embed = new EmbedBuilder();
+                    embed.setAuthor("i don't have manage roles permissions. you can turn those on in role settings <3", null, event.getJDA().getSelfUser().getAvatarUrl());
+                    embed.setColor(Color.red);
+                    event.getMessage().getChannel().sendMessage(embed.build()).queue();
+                }
+
+            }
+            else{
+                EmbedBuilder embed = new EmbedBuilder();
+                embed.setAuthor("you don't have the perms for that command <3", null, event.getJDA().getSelfUser().getAvatarUrl());
+                embed.setColor(Color.red);
+                event.getMessage().getChannel().sendMessage(embed.build()).queue();
+            }
 
         }
     }
