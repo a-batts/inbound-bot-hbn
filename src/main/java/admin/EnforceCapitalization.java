b04273c0ba45f9bd27dbbda3b99809ba8ab4d1cb
Alex Batts <alex.batts05@gmail.com>
2021-07-10 23:27:51 -0400

    ignorecaps

--- src/main/java/admin/EnforceCapitalization.java
+++ src/main/java/admin/EnforceCapitalization.java
@@ -1,10 +1,13 @@
 package admin;
 
 import main.Main;
+import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+import java.util.Objects;
 
 public class EnforceCapitalization extends ListenerAdapter {
 
@@ -16,11 +19,11 @@ public class EnforceCapitalization extends ListenerAdapter {
         if (showingCaps){
             if (message.getContentRaw().equals(message.getContentRaw().toUpperCase()) && message.getContentRaw().replaceAll("\\s", "").matches("([a-zA-Z]){7,}"))
                 message.reply("no caps in " + message.getChannel().getName() + " please <3").queue();
-            if (message.getContentRaw().equals(Main.COMMAND_PREFIX + "ignorecaps")){
+            if (message.getContentRaw().equals(Main.COMMAND_PREFIX + "ignorecaps") && Objects.requireNonNull(message.getMember()).hasPermission(Permission.MESSAGE_MANAGE)){
                 showingCaps = false;
                 message.reply("i'll start ignoring caps <3").queue();
             }
-            if (message.getContentRaw().equals(Main.COMMAND_PREFIX + "unignorecaps")){
+            if (message.getContentRaw().equals(Main.COMMAND_PREFIX + "unignorecaps") && Objects.requireNonNull(message.getMember()).hasPermission(Permission.MESSAGE_MANAGE)){
                 showingCaps = true;
                 message.reply("no longer ignoring caps <3").queue();
             }
@@ -30,10 +33,21 @@ public class EnforceCapitalization extends ListenerAdapter {
     @Override
     public void onSlashCommand(SlashCommandEvent event){
         if (event.getName().equals("ignorecaps")){
-            showingCaps = false;
-            event.reply("i'll start ignoring caps <3").queue();
+            if (Objects.requireNonNull(event.getMember()).hasPermission(Permission.MESSAGE_MANAGE)){
+                showingCaps = false;
+                event.reply("i'll start ignoring caps <3").queue();
+            }
+            else
+                event.reply("you don't have perms for that <3").queue();
+
         }
         else if (event.getName().equals("unignorecaps")){
+            if (Objects.requireNonNull(event.getMember()).hasPermission(Permission.MESSAGE_MANAGE)){
+                showingCaps = true;
+                event.reply("no longer ignoring caps <3").queue();
+            }
+            else
+                event.reply("you don't have perms for that <3").queue();
 
         }
     }
