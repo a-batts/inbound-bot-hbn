1f2543f6f2e51155915fd719f98509f4ce49dd33
Alex Batts <alex.batts05@gmail.com>
2022-04-13 18:37:34 -0400

    Permission manager

---
+++ src/main/java/com/abatts/inboundbot/permission/PermissionManager.java
@@ -0,0 +1,50 @@
+package com.abatts.inboundbot.permission;
+
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+
+import java.awt.*;
+
+public class PermissionManager {
+    /**
+     * Check if user has specified permission
+     * @param permission
+     * @param member
+     * @return
+     */
+    public static boolean hasPermission(Permission permission, Member member){
+        try{
+            return member.hasPermission(permission);
+        } catch (NullPointerException e){
+            return false;
+        }
+    }
+
+    public static boolean canManageMessages(Member member){
+        return hasPermission(Permission.MESSAGE_MANAGE, member);
+    }
+
+    public static boolean canManageRoles(Member member){
+        return hasPermission(Permission.MANAGE_ROLES, member);
+    }
+
+    public static boolean canMute(Member member){
+        return canManageRoles(member);
+    }
+
+    public static boolean canWarn(Member member) { return canMute(member); }
+
+    public static boolean isAdmin(Member member){
+        return hasPermission(Permission.MANAGE_SERVER, member);
+    }
+
+    public static void throwIncorrectPermsWarning(GuildMessageReceivedEvent event, MessageChannel channel){
+        EmbedBuilder embed = new EmbedBuilder()
+                .setAuthor("You don't have right the permissions for that command", null, event.getJDA().getSelfUser().getAvatarUrl())
+                .setColor(Color.red);
+        channel.sendMessageEmbeds(embed.build()).queue();
+    }
+}
