8ff45f21a6ec2657d23db8e221b1501d8c592531
Alex Batts <alex.batts05@gmail.com>
2022-04-14 11:23:48 -0400

    Removed unnecessary if clause in MuteCommand

--- src/main/java/com/abatts/inboundbot/command/commands/admin/MuteCommand.java
+++ src/main/java/com/abatts/inboundbot/command/commands/admin/MuteCommand.java
@@ -21,49 +21,47 @@ public class MuteCommand implements Command {
     public void runCommand(GuildMessageReceivedEvent event) {
         Message message = event.getMessage();
 
-        if (message.getContentRaw().startsWith(Bot.COMMAND_PREFIX + name)){
-            if (PermissionManager.canManageRoles(message.getMember())) {
-                List<Member> members = message.getMentionedMembers();
-                try {
-                    Role muteRole = event.getGuild().getRolesByName(Bot.MUTE_ROLE, true).get(0);
-                    for (Member member : members){
-                        if (! member.getUser().isBot())
-                            event.getGuild().addRoleToMember(member, muteRole).queue();
-                        else{
-                            if (members.size() == 1){
-                                EmbedBuilder embed = new EmbedBuilder()
-                                        .setAuthor("I can't mute myself", null, event.getJDA().getSelfUser().getAvatarUrl())
-                                        .setColor(Color.red);
-                                message.getChannel().sendMessageEmbeds(embed.build()).queue();
-                                return;
-                            }
+        if (PermissionManager.canManageRoles(message.getMember())) {
+            List<Member> members = message.getMentionedMembers();
+            try {
+                Role muteRole = event.getGuild().getRolesByName(Bot.MUTE_ROLE, true).get(0);
+                for (Member member : members){
+                    if (! member.getUser().isBot())
+                        event.getGuild().addRoleToMember(member, muteRole).queue();
+                    else{
+                        if (members.size() == 1){
+                            EmbedBuilder embed = new EmbedBuilder()
+                                    .setAuthor("I can't mute myself", null, event.getJDA().getSelfUser().getAvatarUrl())
+                                    .setColor(Color.red);
+                            message.getChannel().sendMessageEmbeds(embed.build()).queue();
+                            return;
                         }
                     }
-
-                    EmbedBuilder embed = new EmbedBuilder()
-                            .setAuthor("Successfully muted the mentioned members", null, event.getJDA().getSelfUser().getAvatarUrl())
-                            .setColor(Color.green);
-                    message.getChannel().sendMessageEmbeds(embed.build()).queue();
-                } catch (IndexOutOfBoundsException e) {
-                    EmbedBuilder embed = new EmbedBuilder()
-                            .setAuthor("You don't have a role named `\"" + Bot.MUTE_ROLE + "\"`.", null, event.getJDA().getSelfUser().getAvatarUrl())
-                            .setColor(Color.red);
-                    message.getChannel().sendMessageEmbeds(embed.build()).queue();
-                } catch (InsufficientPermissionException e) {
-                    EmbedBuilder embed = new EmbedBuilder()
-                            .setAuthor("I don't have have `Manage Messages` permissions so I can't run this command", null, event.getJDA().getSelfUser().getAvatarUrl())
-                            .setColor(Color.red);
-                    message.getChannel().sendMessageEmbeds(embed.build()).queue();
-                } catch (HierarchyException e) {
-                    EmbedBuilder embed = new EmbedBuilder()
-                            .setAuthor("I don't have permissions to muter users. Make sure that the mute role is not higher than my highest role", null, event.getJDA().getSelfUser().getAvatarUrl())
-                            .setColor(Color.red);
-                    message.getChannel().sendMessageEmbeds(embed.build()).queue();
                 }
+
+                EmbedBuilder embed = new EmbedBuilder()
+                        .setAuthor("Successfully muted the mentioned members", null, event.getJDA().getSelfUser().getAvatarUrl())
+                        .setColor(Color.green);
+                message.getChannel().sendMessageEmbeds(embed.build()).queue();
+            } catch (IndexOutOfBoundsException e) {
+                EmbedBuilder embed = new EmbedBuilder()
+                        .setAuthor("You don't have a role named `\"" + Bot.MUTE_ROLE + "\"`.", null, event.getJDA().getSelfUser().getAvatarUrl())
+                        .setColor(Color.red);
+                message.getChannel().sendMessageEmbeds(embed.build()).queue();
+            } catch (InsufficientPermissionException e) {
+                EmbedBuilder embed = new EmbedBuilder()
+                        .setAuthor("I don't have have `Manage Messages` permissions so I can't run this command", null, event.getJDA().getSelfUser().getAvatarUrl())
+                        .setColor(Color.red);
+                message.getChannel().sendMessageEmbeds(embed.build()).queue();
+            } catch (HierarchyException e) {
+                EmbedBuilder embed = new EmbedBuilder()
+                        .setAuthor("I don't have permissions to muter users. Make sure that the mute role is not higher than my highest role", null, event.getJDA().getSelfUser().getAvatarUrl())
+                        .setColor(Color.red);
+                message.getChannel().sendMessageEmbeds(embed.build()).queue();
             }
-            else
-                PermissionManager.throwIncorrectPermsWarning(event, message.getChannel());
         }
+        else
+            PermissionManager.throwIncorrectPermsWarning(event, message.getChannel());
     }
 
     @Override
